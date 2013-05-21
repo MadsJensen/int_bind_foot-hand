0a830cf9eb8fad0e858c8a0d1fb8f0ac49efe121
Mads Jensen <mje.mads@gmail.com>
2013-05-22 00:07:21 +0200

    updated describtive_stats.R, added plots_libet.R

--- describtive_stats.R
+++ describtive_stats.R
@@ -21,17 +21,68 @@ by(data_all_long$time,
 
 
 options(digits=4)
-all_mean <- as.data.frame(tapply(data_all_long$time, 
+mean_table <- as.data.frame(tapply(data_all_long$time, 
        data.frame(data_all_long$subid, data_all_long$condition, 
                   data_all_long$modality),
        mean,
        na.rm=TRUE
 ))
 
-all_sd <- as.data.frame(tapply(data_all_long$time, 
+sd_table <- as.data.frame(tapply(data_all_long$time, 
        data.frame(data_all_long$subid, data_all_long$condition, 
                   data_all_long$modality),
        sd,
        na.rm=TRUE
 ))
+
+all_mean_long <- aggregate(data_all_long$time, 
+               by=data.frame(data_all_long$subid, data_all_long$condition, 
+                          data_all_long$modality),
+               mean,
+               na.rm=TRUE
+)
+names(all_mean_long) <- c("subid", "condition", "modality", "mean")
+
+
+#### REMOVE OUTLIERS ####
+data_all_long_no_outlier <- subset(data_all_long, 
+         data_all_long$time > -500 & data_all_long$time < 500)
+
+
+all_mean_long_no_outlier <- aggregate(data_all_long_no_outlier$time, 
+                           by=data.frame(data_all_long_no_outlier$subid, 
+                                         data_all_long_no_outlier$condition, 
+                                         data_all_long_no_outlier$modality),
+                           mean,
+                           na.rm=TRUE
+)
+names(all_mean_long_no_outlier) <- c("subid", "condition", "modality", "mean")
+
+
+by(data_all_long_no_outlier$time, 
+   data_all_long_no_outlier[, c("subid", "modality", "condition")], 
+   stat.desc, basic=F, norm=T)
+
+
+options(digits=4)
+mean_table_no_outlier <- as.data.frame(tapply(data_all_long_no_outlier$time, 
+                                 data.frame(data_all_long_no_outlier$subid, 
+                                            data_all_long_no_outlier$condition, 
+                                            data_all_long_no_outlier$modality),
+                                 mean,
+                                 na.rm=TRUE
+))
+
+sd_table_no_outlier  <- as.data.frame(tapply(data_all_long_no_outlier$time, 
+                                             data.frame(data_all_long_no_outlier$subid, 
+                                                        data_all_long_no_outlier$condition, 
+                                                        data_all_long_no_outlier$modality),
+                                             sd,
+                                             na.rm=TRUE
+))
+
+mean_diff_table <- mean_table - mean_table_no_outlier
+sd_diff_table <- sd_table - sd_table_no_outlier
+
+
 
---
+++ plots_libet.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+
+
+ggplot(data_all_long_no_outlier, aes(subid, time, colour=condition)) + 
+  geom_boxplot() + 
+   coord_flip() +
+  facet_wrap(~modality)
